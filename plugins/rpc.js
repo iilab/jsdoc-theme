0e18113e4621e4fc2abfd8ff90f6e57e63f2c794
Jun Matsushita <jun@iilab.org>
2017-05-18 17:36:46 +0200

    Added @rpcroute and @rpccall and further customised

--- plugins/rpc.js
+++ plugins/rpc.js
@@ -1,9 +1,25 @@
 exports.defineTags = function(dictionary) {
-    dictionary.defineTag('rpc', {
-      onTagged: handleRpcTag
-    });
+  dictionary.defineTag('rpc', {
+    onTagged: handleRpcTag
+  });
+  dictionary.defineTag('rpcroute', {
+    onTagged: handleRpcRoute
+  });
+  dictionary.defineTag('rpccall', {
+    onTagged: handleRpcCallTag
+  });
 };
 
 function handleRpcTag(doclet, tag) {
   doclet.rpc = tag.value || true;
+}
+
+function handleRpcRoute(doclet, tag) {
+  doclet.rpcroute = tag.value || true;
+}
+
+function handleRpcCallTag(doclet, tag) {
+  if (!tag.value) return;
+  doclet.rpccall = doclet.rpccall || [];
+  doclet.rpccall.push(tag);
 }
